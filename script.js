d8fa667a3cc226ac5cbc429795df057f79b5b004
DontMindIfIRepo <thoppakkunj@gmail.com>
2026-07-12 20:13:09 -0500

    added datetime to make user comfy

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 const button = document.querySelector(".btn-prs-m");
 const welcome = document.querySelector("#welcome");
+const aftersetup = document.querySelector(".aft-setup");
+const now = new Date();
+const datetime = document.querySelector("#datetime");
 let user = localStorage.getItem("user");
-
-if (user !== null) {
-    button.style.display = "none";
-    welcome.textContent = "Hi, " + user + "!";
+let visits = Number(localStorage.getItem("visits") || 0);
+function updateClock() {
+    const now = new Date();
+    datetime.textContent = now.toLocaleString();
 }
+aftersetup.style.display = "none";
+if (user !== null) {
+    aftersetup.style.display = "";
+    visits++;
+
+    localStorage.setItem("visits", visits);
+
+    button.style.display = "none";
+    welcome.textContent = "Hi, " + user + "! Visit #" + visits;
+}
+
 button.addEventListener("click", () => {
     user = prompt("What is your name?");
+
     if (user !== null && user.trim() !== "") {
         localStorage.setItem("user", user);
+
+        localStorage.setItem("visits", 1);
+
         button.style.display = "none";
-        welcome.textContent = "Hi, " + user + "!";
+        welcome.textContent = "Hi, " + user + "! Visit #1";
     }
 });
+updateClock();
+setInterval(updateClock, 1000);
